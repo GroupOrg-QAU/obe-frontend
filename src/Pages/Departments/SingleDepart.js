93620709fd4e41e4750d88872db4263d60458864
frazchohan007@gmail.com <frazchohan007@gmail.com>
2023-01-03 11:18:49 -0800

    modal file added check it if applicable other use your modal which is much responve than mine

--- src/Pages/Departments/SingleDepart.js
+++ src/Pages/Departments/SingleDepart.js
@@ -1,8 +1,19 @@
-import React from "react";
-
+import React, { useState } from "react";
 export default function SingleDepart(props) {
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  // Edit Modal Function
+  const onEditHandler = () => {
+    setShowEditModal(true);
+    console.log("The Value of Edit Modal is", showEditModal);
+  };
+  //Delete Modal
+  const onDeleteHander = () => {
+    setShowDeleteModal(true);
+    console.log("The Value of Delete Modal is", showDeleteModal);
+  };
   return (
-    <form className="container-fluid w-100 form-inline ms-5 ">
+    <div className="container-fluid w-100 form-inline ms-5 ">
       <div className="table-responsive ">
         <table className="table table-striped ">
           <tbody>
@@ -11,16 +22,18 @@ export default function SingleDepart(props) {
               <td className="col-3">{props.deptName}</td>
               <td className="col-3">{props.schoolName}</td>
               <td className="col-2">{props.deptShortName}</td>
-              <td className="col-2">{props.deptStatus}</td>
+              <td className="col-1">{props.deptStatus}</td>
               <td className="col-2 ">
                 <button
+                  onClick={onEditHandler}
                   className="border border-0  fs-5"
                   style={{ color: "#0B5ED7" }}
                 >
                   <i className="fa-solid fa-pen-to-square"></i>
                 </button>
                 <button
-                  className="border border-0  fs-5 "
+                  onClick={onDeleteHander}
+                  className="border border-0  fs-5 ms-2 "
                   style={{ color: "#0B5ED7" }}
                 >
                   <i className="fa-solid fa-trash-can"></i>
@@ -30,6 +43,6 @@ export default function SingleDepart(props) {
           </tbody>
         </table>
       </div>
-    </form>
+    </div>
   );
 }
