8ef6a6c3ec812b5b3a33ea69cace1c928e3bb0e9
fazal-a- <thefazalabbas@gmail.com>
2023-01-03 15:03:30 +0500

    Done basic routing. will enrich it when redux aded

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import React from "react";
 import { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Header/Navbar";
 import profile from "./Assests/profile.png";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer";
 import Departments from "./Pages/Departments/Departments";
 
-// import Login from "./Pages/Login/Login";
-// import SignUp from "./Pages/Login/SignUp";
-
+import Login from "./Pages/Login/Login";
 function App() {
   return (
-    <Fragment>
-      {/* <Login />
-      <SignUp /> */}
-      <Fragment>
-        {/* <Login />
-      <SignUp /> */}
-        <Navbar title="Q-OBE" userName="Muhammad Afraz" profile={profile} />
-        <main style={{ display: "flex" }}>
-          <Sidebar />
-
-          <div>
-            <Departments />
-          </div>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </Fragment>
-    </Fragment>
+    <Routes>
+      <Route path="/auth" element={<Login />}></Route>
+      <Route
+        path="/departments"
+        element={
+          <Fragment>
+            <Navbar title="Q-OBE" userName="Muhammad Afraz" profile={profile} />
+            <main style={{ display: "flex" }}>
+              <Sidebar />
+              <div>
+                <Departments />
+              </div>
+            </main>
+            <footer>
+              <Footer />
+            </footer>
+          </Fragment>
+        }
+      ></Route>
+    </Routes>
   );
 }
 
